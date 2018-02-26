b5605be5f64544304f80dea94a832ea77e2ea856
burmistrov <upworkvdvjekanavi39@gmail.com>
2018-02-26 17:08:08 +0300

    #89 - remove disable for location caching

--- app/config/reduxPersist.js
+++ app/config/reduxPersist.js
@@ -5,7 +5,7 @@ export default {
   reducerVersion: '4',
   storeConfig: {
     storage: AsyncStorage,
-    blacklist: ['login', 'search', 'location'], // reducer keys that you do NOT want stored to persistence here
+    blacklist: ['login', 'search'], // reducer keys that you do NOT want stored to persistence here
     // whitelist: [], Optionally, just specify the keys you DO want stored to
     // persistence. An empty array means 'don't store any reducers' -> infinitered/ignite#409
   },
